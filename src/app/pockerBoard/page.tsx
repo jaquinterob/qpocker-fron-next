83ed84afde0ccac82c9fed223375732a3de1c50e
John A. Quintero <quinteroberrio@gmail.com>
2024-03-15 23:14:39 -0500

    logOut & changeName added

--- src/app/pockerBoard/page.tsx
+++ src/app/pockerBoard/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import CachedIcon from "@material-ui/icons/Cached";
+import { Theme, Tooltip, withStyles } from "@material-ui/core";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
@@ -19,6 +22,14 @@ export default function PockerBoard() {
   });
   const router = useRouter();
   const user = window.localStorage.getItem(APP.USER) || "";
+  const LightTooltip = withStyles((theme: Theme) => ({
+    tooltip: {
+      backgroundColor: theme.palette.common.white,
+      color: "black",
+      boxShadow: theme.shadows[1],
+      fontSize: 11,
+    },
+  }))(Tooltip);
 
   useEffect(() => {
     sendNewVote();
@@ -33,16 +44,20 @@ export default function PockerBoard() {
       setVotes(votes);
     });
 
-    const newVote: Vote = {
-      user,
-      value: 0,
-    };
-    socket.emit("newVote", newVote, roomParam);
+    registerFirsVote();
 
     return () => {
       socket.off("votes");
     };
   }, []);
+
+  const registerFirsVote = () => {
+    const newVote: Vote = {
+      user,
+      value: 0,
+    };
+    socket.emit("newVote", newVote, roomParam);
+  };
 
   const sendNewVote = () => {
     const newVote: Vote = {
@@ -52,8 +67,33 @@ export default function PockerBoard() {
     socket.emit("newVote", newVote, roomParam);
   };
 
+  const logOut = () => {
+    socket.emit("logOut", user, roomParam);
+    localStorage.removeItem(APP.USER);
+    router.push(URLS.SERVER);
+  };
+
+  const changeUserName = () => {
+    socket.emit("logOut", user, roomParam);
+    localStorage.removeItem(APP.USER);
+    router.push(URLS.SERVER + "selectUser?room=" + roomParam);
+  };
+
   return (
     <div className="fade-in">
+      <div onClick={logOut} className="flex  justify-end p-2">
+        <LightTooltip title="Salir de la sala" placement="left">
+          <ExitToAppIcon className="cursor-pointer" />
+        </LightTooltip>
+      </div>
+      <div className="text-center pt-4 font-bold text-4xl">
+        {user}
+        <span onClick={changeUserName}>
+          <LightTooltip title="cambiar nombre" placement="right">
+            <CachedIcon fontSize="large" className="cursor-pointer pl-2 " />
+          </LightTooltip>
+        </span>
+      </div>
       <div className="flex gap-2 flex-col pt-10 m-auto w-[90%] md:w-[60%] lg:w-[40%] text-lg font-bold ">
         {votes.map((vote) => (
           <VoteCard key={vote.user} vote={vote} />
