cf1a2277df65242ef134e8d07d5db0909f861ed6
John A. Quintero <quinteroberrio@gmail.com>
2024-03-17 23:43:07 -0500

    my name added softly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      <div className=" h-[10vh] fixed bottom-0 align-bottom fade-in m-auto w-full text-center flex justify-center">
-        <img src="/images/celerix.png" alt="celerix image" />
+      <div className="-z-10 md:z-10 cursor-none transition fixed bottom-0 align-bottom fade-in m-auto w-full text-center flex justify-center flex-col text-transparent hover:text-gray-800">
+        <div className=" -mb-3 ">Powered by <strong>JohnQ</strong> on:</div>
+        <img className="h-[10vh] m-auto" src="/images/celerix.png" alt="celerix image" />
       </div>
     </html>
   );
