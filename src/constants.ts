eba6d319c59d44aec9d016ed8f0f27c33da927c7
John A. Quintero <quinteroberrio@gmail.com>
2024-03-20 22:53:18 -0500

    constant ubication updated

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+export namespace APP {
+  export const USER = "user";
+}
+
+export namespace URLS {
+  export const SOCKET = "https://socket.jaquinterob.com/";
+  // export const SOCKET = "http://localhost:3000/";
+  export const SERVER = "https://qpocker.jaquinterob.com/";
+  // export const SERVER = "http://localhost:3001/";
+}
+
+export namespace ROUTES {
+  export const ROOM = "room";
+}
