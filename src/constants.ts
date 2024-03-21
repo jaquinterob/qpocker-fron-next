8aea6c6b9647862dba02e11ebe0a11a9cefb3a69
John A. Quintero <quinteroberrio@gmail.com>
2024-03-20 23:20:00 -0500

    constants okay

--- src/constants.ts
+++ src/constants.ts
@@ -4,9 +4,7 @@ export namespace APP {
 
 export namespace URLS {
   export const SOCKET = "https://socket.jaquinterob.com/";
-  // export const SOCKET = "http://localhost:3000/";
   export const SERVER = "https://qpocker.jaquinterob.com/";
-  // export const SERVER = "http://localhost:3001/";
 }
 
 export namespace ROUTES {
